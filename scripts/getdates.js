b0d200a0afbe6106c6a1aabb7970967f5b4b732a
Chad Miller <chadhartmiller@gmail.com>
2024-04-26 11:35:05 -0600

    added week 1 assign

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Get current year and populate in footer
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+// Get last modified date and populate in footer
+document.getElementById('lastModified').textContent = "Last Modified: " + document.lastModified;
