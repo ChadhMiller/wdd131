a6175a438c2e5e445e87d25641ac83c04f5a33c0
Chad Miller <chadhartmiller@gmail.com>
2024-04-29 13:51:53 -0600

    added bom app

---
+++ week02/scripts/bom.js
@@ -0,0 +1,10 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('list');// I need to fill in the blank
+const li = document.createElement('li');
+const deleteButton = document.createElement('button');
+
+li.textContent = input.value;
+deleteButton.textContent = 'X';
+li.append(deleteButton);
+list.append(li);
